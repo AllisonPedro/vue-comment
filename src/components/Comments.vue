e93dc01a87170662d9985bb481b2459ba9dd7cc4
Allison Pedro <allisondjkk@gmail.com>
2021-12-27 10:21:05 -0300

    add code

---
+++ src/components/Comments.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+            <h1>Comentarios</h1>
+            <hr>
+            <FormTodo v-on:add-todo="addComment"/>
+            <hr>
+            <div class="list-group">
+                <div class="list-group-item" v-for="com, index in allComments" :key="index">
+                    <span class="comment__autor">Autor: <strong>{{com.name}}</strong></span>
+                    <p>{{com.message}}</p>
+                    <div>
+                        <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
+                    </div>
+                </div>
+            </div>
+            <hr>
+    </div>
+</template>
+
+<script>
+import FormTodo from "./FormTodo.vue"
+export default{
+    components:{
+        FormTodo
+    },
+    
+        data(){
+            return{
+                comments:[]//comments
+                
+            } //return 
+        }, //data()
+        methods: {
+            addComment(comment){
+                this.comments.push(comment)
+            },
+
+            removeComment(index){
+                this.comments.splice(index, 1)
+            }//removeComment
+        },//methods
+
+        computed:{
+            allComments(){
+                return this.comments.map(comment => ({
+                    ...comment,
+                    name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+                }))//return
+            }//allComments
+        }//computed
+
+
+}
+
+    
+
+</script>
+
+
+ 
+     
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="form-todo form-group">
+        <p>
+            <input placeholder="Nome" type="text" name="author" class="form-control" v-model="name">
+        </p>
+        <p>
+            <input placeholder="Comentário" type="text" name="message" class="form-control" v-model="message">
+        </p>
+            <button @click="addComment" type="submit" class="btn btn-primary" > Comentar </button>
+    </div>
+</template>
+
+
+<script>
+    export default{
+        data() {
+            return {
+                name: "",
+                message: "",
+            }
+
+        },
+        methods:{
+            addComment(){
+                if(this.message.trim() === ''){
+                    return;
+                } //if
+                
+                this.$emit('add-todo', {
+                    name: this.name,
+                    message: this.message
+                })
+                this.name = ''
+                this.message = ''
+                
+            },//addComment
+        }
+
+    }
+</script>
